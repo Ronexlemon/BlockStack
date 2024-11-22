a7f2a140bd68953f2ccc8a178902744c99ab1e6a
RonexLemon <ronexondimu@gmail.com>
2024-11-22 08:59:17 +0300

    hash data

--- cryptohash/hash.go
+++ cryptohash/hash.go
@@ -3,6 +3,9 @@ package cryptohash
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+
+	"github.com/RonexLemon/chain/types"
 )
 
 
@@ -10,9 +13,15 @@ type Hash struct{
 	HashValue string
 }
 
-func (h *Hash) HashData(data string)string{
+func (h *Hash) HashData(data *types.Block)string{
 	hasher  := sha256.New()
-	hasher.Write([]byte(data))
+	serializedData := fmt.Sprintf("%d%s%s%d",
+		data.Height,
+		data.PrevHash,
+		data.Hash,
+		data.Nonce,
+	)
+	hasher.Write([]byte(serializedData))
 	h.HashValue = hex.EncodeToString(hasher.Sum(nil))
 	return h.HashValue
 }
